e9fd1ef33632e4c66bb862885a53562bc015fe85
Gama Aguilar <gama@9104studios.com>
2016-09-22 10:20:27 -0700

    checkpoint 3 completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,23 @@
-angular.module('blocJams', []);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+        });
+        
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateURL: '/templates/landing.html'
+        })
+            .state('album', {
+                url: '/album', 
+                templateURL: '/templates/album.html'
+        });  
+    }
+    
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
