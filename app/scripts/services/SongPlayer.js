3bffa330fcbaf463a1705abe37c52e6e65c96b19
Gama Aguilar <gama@9104studios.com>
2016-10-02 13:05:27 -0400

    checkpoint 7 complete services 2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,20 +1,69 @@
-(function() {
+(function () {
     function SongPlayer() {
-        var songPlayer = {};
+        var SongPlayer = {};
         
-        SongPlayer.play = function(song) {
-            var currentBuzzObject = new buzz.sound(sound.audioUrl, {
+        var currentSong = null;
+        
+        /* 
+        @desc Buzz object audio file
+        @type {Object}
+        */
+        
+        var currentBuzzOjbect = null;
+        
+        
+        /*
+        var playSong = function() {
+            currentBuzzOjbect.play();
+            song.playing = true;
+        };
+        
+        */
+        
+        /**
+        @function setSong
+        @desc Stops currently playing song and loads new audio file as current
+        @param {Object} song
+        **/
+        
+        var setSong = function (song) {
+            if (currentBuzzOjbect) {
+                currentBuzzOjbect.stop();
+                currentSong.playing = null;
+            }
+            
+            currentBuzzOjbect = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
             });
             
-            currentBuzzObject.play();
-        }
+            currentSong = song;
+        };
+        
+        SongPlayer.play = function (song) {
+            if (currentSong !== song) {
+                setSong(song);
+                currentBuzzOjbect.play();
+                song.playing = true;
+                
+            } else if (currentSong === song) {
+                if (currentBuzzOjbect.isPaused()) {
+                    currentBuzzOjbect.play();
+                }
+            }
+        };
+        
+        SongPlayer.pause = function (song) {
+            currentBuzzOjbect.pause();
+            song.playing = false;
+        };
         
         return SongPlayer;
     }
     
     angular
         .module('blocJams')
-        .factory('SongPlayer')
-})
+        .factory('SongPlayer', SongPlayer);
+    
+    
+})();
